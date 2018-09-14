b4b7e8b8f26163032380cabcc97c4b08f3de1811
Joshua Qualls <jrqualls@iu.edu>
2018-09-14 17:58:49 -0400

    Add files via upload

---
+++ Homework/BuffonNeedle.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+public class BuffonNeedle { 
+  public static double checkBN(){
+    Random rand = new Random() ; 
+    int yLow = rand.nextInt(3) ; 
+    double alpha = Math.toRadians((double)rand.nextInt(181)) ; 
+    int hits = 0 ; 
+    int attempts = 0 ; 
+    double yHigh = yLow + Math.sin(alpha) ; 
+    while (attempts < 10000){ 
+      if(yHigh >= 2) { 
+        hits += 1 ; 
+        attempts += 1 ; 
+        yLow = rand.nextInt(3) ; 
+        alpha = rand.nextInt(181) ; 
+        yHigh = yLow + Math.sin(alpha) ; 
+      } 
+      else { 
+        attempts += 1 ; 
+        yLow = rand.nextInt(3) ; 
+        alpha = rand.nextInt(181) ; 
+        yHigh = yLow + Math.sin(alpha) ; 
+      } 
+    } 
+    return attempts/hits ; 
+  }
+  
+  public static void main(String[] args){
+    System.out.println(checkBN());
+}
+}
